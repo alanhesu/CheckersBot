d45201858311e3b43e88b93f943ba9496aff7c5c
alanhesu <alanhesu@gmail.com>
2018-11-06 16:52:21 -0500

    code from lab

--- getBoardImage.m
+++ getBoardImage.m
@@ -1,30 +1,37 @@
 function board = getBoardImage(im, coord)
 	board = char(8, 8);
-	figure;
-	imshow(im);
+% 	figure;
+% 	imshow(im);
 	[sizeX, sizeY, sizeZ] = size(im);
 	[rowsInImage, columnsInImage] = meshgrid(1:sizeY, 1:sizeX);
 	
-	rMin = 20;
-	rMax = 50;
+	rMin = 7;
+	rMax = 20;
 	im_hsv = rgb2hsv(im);
-	[centers, radii] = imfindcircles(im, [rMin, rMax], 'EdgeThreshold', .1);
-	viscircles(centers, radii, 'Color', 'b');
+	[centers, radii] = imfindcircles(im, [rMin, rMax], 'EdgeThreshold', .1, 'Method', 'TwoStage', 'Sensitivity', .87);
+% 	viscircles(centers, radii, 'Color', 'b');
 	
-	avgRad = mean(radii);
-	for r = 1:8
-		for c = 1:8			
-			board(r,c) = 'x';
-			for i = 1:length(centers(:,1))			
-				if norm(coord{r, c} - centers(i,:)) < avgRad
-					circlePixels = (rowsInImage - centers(i,1)).^2 + (columnsInImage - centers(i,2)).^2 <= radii(i).^2;
-					value = im_hsv(:,:,3);
-					if mean(mean(value(circlePixels))) > .5
-						board(r,c) = 'W';
-					else
-						board(r,c) = 'B';
-				end
-			end
-		end
-	end
+    if ~isempty(radii)
+        avgRad = mean(radii);
+        for r = 1:8
+            for c = 1:8			
+                board(r,c) = 'x';
+                for i = 1:length(centers(:,1))			
+                    if norm(coord{r, c} - centers(i,:)) < avgRad
+                        circlePixels = (rowsInImage - centers(i,1)).^2 + (columnsInImage - centers(i,2)).^2 <= (radii(i)*.7).^2;
+                        hue = im_hsv(:,:,1);
+                        red = im(:,:,1);
+                        blue = im(:,:,3);
+                        if mean(mean(red(circlePixels))) > mean(mean(blue(circlePixels)))
+                            board(r,c) = 'R';
+                            viscircles(centers(i,:), radii(i), 'Color', 'r');
+                        else
+                            board(r,c) = 'B';
+                            viscircles(centers(i,:), radii(i), 'Color', 'b');
+                        end
+                    end
+                end
+            end            
+        end
+    end
 end
--- initBoardImage.m
+++ initBoardImage.m
@@ -1,23 +1,30 @@
 function coord = initBoardImage(im)
-	figure;
-	imshow(im);
-	
-	rMin = 20;
-	rMax = 50;
-	im_bw = im2bw(im, .5);
-	[centers, radii] = imfindcircles(im, [rMin, rMax], 'EdgeThreshold', .1);
-	viscircles(centers, radii, 'Color', 'b');
-	
-	topRight = [max(centers(:,1)), min(centers(:,2))];
-	botLeft = [min(centers(:,1)), max(centers(:,2))];
-	
-	coord = {8, 8};
-	x = linspace(botLeft(1), topRight(1), 8);
-	y = linspace(topRight(2), botLeft(2), 8);
-	for r = 1:8
-		for c = 1:8
-			coord{r, c} = [x(c), y(r)];
-			viscircles(coord{r, c}, 5);
-		end
-	end
+    figure(3);
+    imshow(im);    
+    coord = {8, 8};
+    for r = 1:8
+        for c = 1:8
+            coord{r,c} = [0,0];
+        end
+    end
+
+    rMin = 7;
+    rMax = 20;
+    im_bw = im2bw(im, .5);
+    [centers, radii] = imfindcircles(im, [rMin, rMax], 'EdgeThreshold', .1, 'Method', 'TwoStage', 'Sensitivity', .87);
+    viscircles(centers, radii, 'Color', 'b');
+
+    if length(centers) == 24
+        topRight = [max(centers(:,1)), min(centers(:,2))];
+        botLeft = [min(centers(:,1)), max(centers(:,2))];
+        
+        x = linspace(botLeft(1), topRight(1), 8);
+        y = linspace(topRight(2), botLeft(2), 8);
+        for r = 1:8
+            for c = 1:8
+                coord{r, c} = [x(c), y(r)];
+                viscircles(coord{r, c}, 5);
+            end
+        end
+    end
 end
